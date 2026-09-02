b0fc848ec9962865fe883e742031e0bcec237bdb
Atul Kumar Agrahari <atul.01login@gmail.com>
2026-09-02 23:52:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Hello FastAPI"}
+
+@app.get('/about')
+
+def about():
+    return{'message':'CampusX is an education plateform'}
